969c562b9391fab9e6b6233e38bb08bbb4fa4fc7
MartinsRoberto <robertomartinsramm@gmail.com>
2022-12-03 18:17:08 -0300

    metodo put

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const db = require('./db/connection')
+const express    = require('express')
+const app        = express()
+const db         = require('./db/connection')
+const bodyParser = require('body-parser')
+const routes     = require('./routes/jobs')
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
 db.authenticate().then( () => console.log('Connected to database')).catch( (err) => console.log(err))
 
@@ -8,6 +16,9 @@ app.get('/', (req, res) => {
   res.send('Succeed')
 })
 
+app.use('/jobs', routes)
+
 const port = 3000
 
 app.listen(port, () => console.log(`Connected to port ${port}`))
+
---
+++ routes/jobs.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router  = express.Router()
+const Job     = require('../models/Job')
+
+router.post('/add', (req, res) => {
+  const { title, description, company, salary, email, new_job } = req.body
+
+
+  Job.create({
+    title,
+    description, 
+    company,
+    salary,
+    email,
+    new_job
+  }).then( () => res.redirect('/'))
+
+})
+
+module.exports = router
