c3fde4a18c24e3e83f8d499c9fe0a6b0a36e5bd0
MartinsRoberto <robertomartinsramm@gmail.com>
2022-12-04 16:55:33 -0300

    add busca

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const path       = require('path')
 const db         = require('./db/connection')
 const bodyParser = require('body-parser')
 const routes     = require('./routes/jobs')
+const Job        = require('./models/Job')
+const Sequelize  = require('sequelize')
+const Op         = Sequelize.Op
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+
 
 app.engine('.handlebars', exphbs.engine({ extname: '.handlebars', defaultLayout: "main"}));
 app.set('views', path.join(__dirname, 'views'))
@@ -19,7 +25,36 @@ app.use(express.static(path.join(__dirname, 'public')))
 db.authenticate().then( () => console.log('Connected to database')).catch( (err) => console.log(err))
 
 app.get('/', (req, res) => {
-  res.render('index')
+
+  let search = req.query.job
+
+  // PH >> PHP ou Word >> WordPress
+  let query = '%'+search+'%'
+
+  if(!search){
+    Job.findAll({order: [
+      ['createdAt', 'DESC']
+    ]})
+    .then(jobs => {
+      res.render('index',{
+        jobs
+      })
+  
+    })
+  }
+  else{
+    Job.findAll({
+      where: {title: {[Op.like]: query}},
+      order: [['createdAt', 'DESC']]
+    })
+    .then(jobs => {
+      res.render('index',{
+        jobs,
+        search
+      })
+  
+    })
+  }
 })
 
 app.use('/jobs', routes)
