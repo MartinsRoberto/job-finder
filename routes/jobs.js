2d6c038274f014d9fa2429bbefa177d75a26cab0
MartinsRoberto <robertomartinsramm@gmail.com>
2022-12-04 18:16:56 -0300

    adicionando detalhes das vagas

--- routes/jobs.js
+++ routes/jobs.js
@@ -5,6 +5,15 @@ const Job     = require('../models/Job')
 router.get('/add', (req, res) => {
   res.render('add')
 })
+
+router.get('/view/:id', (req, res) => Job.findOne({
+  where: {id: req.params.id}
+}).then(job => {
+  res.render('view',{
+    job
+  })
+}))
+
 router.post('/add', (req, res) => {
   const { title, description, company, salary, email, new_job } = req.body
 
